79c6713ba057787356b752ddcfa8b9e1b7e5d84d
nikkris <nikkris@gmail.com>
2015-09-10 15:38:38 -0400

    updated python IDE

--- hw/code/1/okok.py
+++ hw/code/1/okok.py
@@ -6,10 +6,10 @@ from ok import *
 
 print time.strftime("%H:%M:%S\n")
 
-# @ok
-# def _ok():
-#   assert 9 == 3
-#
+@ok
+def _ok():
+  assert 9 == 3
+
 @ok
 def _ok1():
   assert 1==1
@@ -20,10 +20,10 @@ def _ok2():
 
 @ok
 def _ok3():
-  assert 3==3 
+  assert 3==3
 
 @ok
 def _ok4():
   assert unittest.tries==4
   assert unittest.fails==1
-  print unittest.score() 
+  print unittest.score()
