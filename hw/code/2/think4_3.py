55c03c557a4d803052bdc866050c1e326f8dae70
nikkris <nikkris@gmail.com>
2015-09-15 20:28:07 -0400

    4.3

--- hw/code/2/think4_3.py
+++ hw/code/2/think4_3.py
@@ -1,31 +1,38 @@
 __author__ = "Nikhil Krishnan"
-# problem 4.2
+# problem 4.3
 
 from swampy.TurtleWorld import *
 from polygon import *
 import math
 
-def petal(t, r, angle):
-    for i in range(2):
-        arc(t, r, angle)
-        lt(t, 180-angle)
+def isotriangle(tur,isoside,isoangle):
+    otherside = 2*isoside*math.sin((isoangle/2.0)/360.0*2*math.pi)
+    fd(tur,isoside)
+    lt(tur,90+isoangle/2.0)
+    fd(tur,otherside)
+    lt(tur,90+isoangle/2.0)
+    fd(tur,isoside)
 
+def pie(tur,r,n):
+    isoangle = float(360.0/n)
+    rt(tur,isoangle/2)
+    for lp in range(n):
+        isotriangle(tur,r,isoangle)
+        rt(tur,180)
+    
 
-
-def flower(t, n, r, angle):
-    for i in range(n):
-        petal(t, r, angle)
-        lt(t, 360.0/n)
 
 
 
 world = TurtleWorld()
 bob = Turtle()
-bob.delay = 0.01
+bob.delay = 0
 print bob
 
-# flower(bob, 7, 60.0, 60.0)
-# flower(bob, 10, 40.0, 80.0)
-flower(bob, 20, 140.0, 20.0)
+# pie(bob,100,5) # first figure
+# pie(bob,100,6) # second figure
+pie(bob,100,7) # third figure
+
+die(bob)
 
 wait_for_user()
